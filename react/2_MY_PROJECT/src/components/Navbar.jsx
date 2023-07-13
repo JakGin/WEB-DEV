e7f0671fc2357dd30c74923607e2eda018b0c897
Jakub <jakubglinka9@gmail.com>
2023-07-13 13:38:09 +0200

    Add AirBnB project

---
+++ react/2_MY_PROJECT/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "/src/styles/navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/images/airbnb-logo.png" alt="AirBnB logo" width="100" />
+    </nav>
+  )
+}
+
+export default Navbar;

0c71fb8357788ab4e712d978d840aa67b3a59e0f
Jakub <jakubglinka9@gmail.com>
2023-07-17 13:06:27 +0200

    Add light theme to the project

--- react/0_React/src/App.jsx
+++ react/0_React/src/App.jsx
@@ -3,10 +3,24 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
+  React.useEffect(() => {
+    if (darkMode) {
+      document.querySelector("body").style.backgroundColor = "#282d35"
+    } else {
+      document.querySelector("body").style.backgroundColor = "white"
+    }
+  })
+
   return (
-    <div className="container">
-      <Navbar />
-      <Main />
+    <div className={darkMode ? "container dark-c" : "container"}>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }

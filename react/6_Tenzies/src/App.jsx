7ac92dbbbf13c1c1c32d0e123ced27c91ada7a36
Jakub <jakubglinka9@gmail.com>
2023-07-20 13:43:09 +0200

    Make the dice change color on click

--- react/6_Tenzies/src/App.jsx
+++ react/6_Tenzies/src/App.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const nOfDices = 10;
-  const [dice, setDice] = React.useState(allNewDice())
-  const diceElements = dice.map(die => (
-    <Die number={die} />
-  ))
+  const [dice, setDice] = React.useState(allNewDice());
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < nOfDices; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
     }
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDice(prevDice => dice.map(die => ({
+      ...die,
+      isHeld: die.id === id ? !die.isHeld : die.isHeld
+    })))
   }
 
   return (
     <div className="container">
       {/* <Header /> */}
-      <div className="dice-container">
-        {diceElements}
-      </div>
-      <div className="button" onClick={rollDice}>Roll</div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="button" onClick={rollDice}>
+        Roll
+      </button>
     </div>
   );
 }

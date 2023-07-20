9fb7122c427232c06ef119762590dc1e45012bb8
Jakub <jakubglinka9@gmail.com>
2023-07-20 11:12:37 +0200

    Simple css and simple componets for the start of the project

--- react/6_Tenzies/src/App.jsx
+++ react/6_Tenzies/src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import "./styles/app.css";
+import Die from "./components/Die";
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Main />
+      {/* <Header /> */}
+      <div className="dies">
+        <Die number={2} />
+        <Die number={4} />
+        <Die number={6} />
+        <Die number={4} />
+        <Die number={1} />
+        <Die number={3} />
+        <Die number={4} />
+        <Die number={4} />
+        <Die number={4} />
+        <Die number={2} />
+      </div>
     </div>
   );
 }

91614b500e87f36eba7242cee2f2bae492248c71
Jakub <jakubglinka9@gmail.com>
2023-07-20 10:33:29 +0200

    Setup for Tenzies

---
+++ react/6_Tenzies/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/app.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;

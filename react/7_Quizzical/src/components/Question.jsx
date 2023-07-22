7e4348ad6c7486a9b4a9357c413f5919f759b28c
Jakub <jakubglinka9@gmail.com>
2023-07-22 12:00:57 +0200

    Add needed style to components

--- react/7_Quizzical/src/components/Question.jsx
+++ react/7_Quizzical/src/components/Question.jsx
@@ -1,9 +1,24 @@
 import React from "react"
+import {decode} from 'html-entities';
 
 export default function Question(props) {
+
+
+  const answersElements = props.allAnswers.map(answer => {
+    return (
+      <div className="answer">
+        {decode(answer)}
+      </div>
+    )
+  })
+
   return (
     <div className="question">
-      {props.allAnswers}
+      <h2>{decode(props.question)}</h2>
+      <div className="answers">
+        {answersElements}
+      </div>
+      <hr />
     </div>
   )
 }

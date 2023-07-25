5c4dc71444ce21a62934ac3b08b462badb84a5ee
Jakub <jakubglinka9@gmail.com>
2023-07-25 14:03:42 +0200

    Add buttons functionality and timeout when switching images

--- js-api/Tindog/Dog.js
+++ js-api/Tindog/Dog.js
@@ -3,12 +3,40 @@ class Dog {
     Object.assign(this, data)
   }
 
+  setHasBeenSwiped(value) {
+    this.hasBeenSwiped = value
+  }
+
+  setHasBeenLiked(value) {
+    this.hasBeenLiked = value
+  }
+
+  getDogBadgeHtml() {
+    const {hasBeenSwiped, hasBeenLiked} = this
+    const dogBadgeUrl = hasBeenSwiped && hasBeenLiked ?
+      "./images/badge-like.png" : hasBeenSwiped ?
+      "./images/badge-nope.png" : ""
+
+    const dogBadgeHtml = dogBadgeUrl ? 
+      `
+        <div class="dog-badge">
+          <img src="${dogBadgeUrl}">
+        </div>
+      `
+      : ""
+
+    return dogBadgeHtml
+  }
+
   getDogHtml() {
     const {name, avatar, age, bio} = this
+    const dogBadgeHtml = this.getDogBadgeHtml()
+    
     return `
       <div class="dog-image">
         <img src="${avatar}" alt="dog image">
       </div>
+      ${dogBadgeHtml}
       <div class="dog-info">
         <div>
           <h2>${name}, ${age}</h2>
--- js-api/Tindog/index.js
+++ js-api/Tindog/index.js
@@ -1,5 +1,57 @@
-import dogs from "./data.js"
-import Dog from "./Dog.js"
+import dogs from "./data.js";
+import Dog from "./Dog.js";
 
-const dog = new Dog(dogs[0])
-document.querySelector(".dog-image-container").innerHTML = dog.getDogHtml()
+function like() {
+  if (!clickAllowed) return;
+  clickAllowed = false;
+
+  document.querySelector(".like-button").style.backgroundColor = "#DBFFF4";
+  setTimeout(() => {
+    currentDog.setHasBeenSwiped(true);
+    currentDog.setHasBeenLiked(true);
+    document.querySelector(".like-button").style.backgroundColor = "#fff";
+    displayNewDog();
+    clickAllowed = true;
+  }, 1000);
+}
+
+function nope() {
+  if (!clickAllowed) return;
+
+  clickAllowed = false;
+
+  document.querySelector(".cross-button").style.backgroundColor = "#FFE7EF";
+  setTimeout(() => {
+    currentDog.setHasBeenSwiped(true);
+    currentDog.setHasBeenLiked(false);
+    document.querySelector(".cross-button").style.backgroundColor = "#fff";
+    displayNewDog();
+    clickAllowed = true;
+  }, 1000);
+}
+
+function displayNewDog() {
+  if (currentDogId >= dogsToDisplay.length) {
+    currentDogId = 0;
+  }
+  currentDog = dogsToDisplay[currentDogId];
+  currentDogId++;
+  document.querySelector(".dog-image-container").innerHTML =
+    currentDog.getDogHtml();
+}
+
+function createDogObjects() {
+  return dogs.map((dog) => {
+    return new Dog(dog);
+  });
+}
+
+let dogsToDisplay = createDogObjects();
+let currentDog = {};
+let currentDogId = 0;
+let clickAllowed = true;
+
+document.querySelector(".cross-button").addEventListener("click", nope);
+document.querySelector(".like-button").addEventListener("click", like);
+
+displayNewDog();

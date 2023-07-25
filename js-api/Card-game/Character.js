630091b4f789c44048b97a919a69875455f32ce8
Jakub <jakubglinka9@gmail.com>
2023-07-25 11:33:32 +0200

    CSS changes so the layout displays correctly

--- js-api/Card-game/Character.js
+++ js-api/Card-game/Character.js
@@ -1,30 +1,50 @@
-import { getDiceRollArray } from "./utils.js";
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
-function Character(data) {
-  Object.assign(this, data);
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+    }
 
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount)
-      .map(function (num) {
-        return `<div class="dice">${num}</div>`;
-      })
-      .join("");
-  };
+    setDiceHtml() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map((num) =>
+            `<div class="dice">${num}</div>`).join("")
+    }
 
-  this.getCharacterHtml = function () {
-    const { name, avatar, health, diceCount } = this;
-    let diceHtml = this.getDiceHtml(diceCount);
+    takeDamage(attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore
+        if (this.health <= 0) {
+            this.dead = true
+            this.health = 0
+        }
+    }
 
-    return `
-      <div class="character-card">
-          <h4 class="name"> ${name} </h4>
-          <img class="avatar" src="${avatar}" />
-          <div class="health">health: <b> ${health} </b></div>
-          <div class="dice-container">
-              ${diceHtml}
-          </div>
-      </div>`;
-  };
+    getHealthBarHtml() {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`
+    }
+
+    getCharacterHtml() {
+        const { name, avatar, health, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`
+    }
 }
 
-export default Character;
+export default Character

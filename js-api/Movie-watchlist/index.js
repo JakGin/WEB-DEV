23590b313f7bb4036dbe6838c3230160aae92e37
Jakub <jakubglinka9@gmail.com>
2023-07-28 13:00:57 +0200

    Done styling

--- js-api/Movie-watchlist/index.js
+++ js-api/Movie-watchlist/index.js
@@ -1,4 +1,4 @@
-const API_KEY = "1a5ffb62a"
+const API_KEY = "1a5ffb62"
 
 async function getMovies(movie) {
   const res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${movie}`)
@@ -15,25 +15,59 @@ async function getAdditionalMovieData(movie) {
   return data
 }
 
-function setMoviesHtml(movies) {
+function setMovieHtml(movies) {
+  document.querySelector(".movies").innerHTML += `
+    <div class="movie">
+      <img class="poster" src="${movies.Poster}" />
+      <header>
+        <h2 class="movie-title">${movies.Title}</h2>
+        <img src="img/star.svg" />
+        ${movies.Rating}
+      </header>
+      <p class="run-time">${movies.Runtime}</p>
+      <p class="genre">${movies.Genre}</p>
+      <button class="add-btn">
+        <img src="img/black-plus.svg" />
+        <p>Watchlist</p>
+      </button>
+      <p class="plot">${movies.Plot}</p>
+    </div>
+  `
+}
+
+function filterMovies(movies) {
+  return movies.filter(movie => movie.Poster !== "N/A")
+}
+
+function renderDefaultPage() {
   document.querySelector(".movies").innerHTML = `
-    <div class="movie">${movies}</div>
+  <div class="placeholder">
+    <img src="img/placeholder.svg" alt="placeholder" width="200" height="200" />
+    <p>Start exploring</p>
+  </div>
   `
 }
 
 document.querySelector("form").addEventListener("submit", async (event) => {
   event.preventDefault()
+  document.querySelector(".movies").innerHTML = ""
   const movie = document.querySelector("form input").value
-  data = await getMovies(movie)
+  if (movie === "") {
+    renderDefaultPage()
+    return
+  }
+  let data = await getMovies(movie)
   if (data !== "Movie Not Found") {
+    data = filterMovies(data)
     data.forEach(async (movie) => {
       const additionalData = await getAdditionalMovieData(movie.Title)
       movie.Genre = additionalData.Genre
       movie.Runtime = additionalData.Runtime
       movie.Plot = additionalData.Plot
       movie.Rating = additionalData.Ratings[0]?.Value
+      setMovieHtml(movie)
     })
-    setMoviesHtml(data)
+    
   } else {
     document.querySelector(".movies").innerHTML = `
       <div class="movie-not-found"><p>Unable to find what you're looking for. Please try another search.</p></div>

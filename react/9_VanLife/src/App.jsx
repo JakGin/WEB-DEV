df4166292cae1d8f9e83b4d6ed7e37738641f21c
Jakub <jakubglinka9@gmail.com>
2023-08-02 14:15:15 +0200

    Add a netlify CLI to repo

--- react/9_VanLife/src/App.jsx
+++ react/9_VanLife/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import "./index.css"
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
         <nav>
           <Link to="/about">About</Link>
         </nav>
@@ -19,6 +20,6 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 export default App;
